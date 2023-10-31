d8b186c8cd778518c1cd7250795c2b346cd5a5e8
梦晞 <2677933891@qq.com>
2023-10-31 22:32:54 +0800

    GDI+ 绘制透明png图片

--- 2023_10_WinTest_1/WinTest_01_ImageLoad.cpp
+++ 2023_10_WinTest_1/WinTest_01_ImageLoad.cpp
@@ -16,24 +16,10 @@ void MessageProc_WM_CREATE(HWND hWnd)
 
 void MessageProc_WM_PAINT(HWND hWnd)
 {
-	class mygdiplus
-	{
-		GdiplusStartupInput gdiplusStartupInput;
-		ULONG_PTR   gdiplusToken;
-	public:
-		mygdiplus()
-		{
-			GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-		}
-		~mygdiplus()
-		{
-			GdiplusShutdown(gdiplusToken);
-		}
-	};
-	mygdiplus Mygdiplus;
-	//————————————————
-	//版权声明：本文为CSDN博主「qiuchangyong」的原创文章，遵循CC 4.0 BY - SA版权协议，转载请附上原文出处链接及本声明。
-	//原文链接：https ://blog.csdn.net/qiuchangyong/article/details/7790517
+	//gdi+绘图初始化
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR   gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 
 	TCHAR szBuffer[1024] = TEXT("");
 	PAINTSTRUCT ps = { 0 };
@@ -42,7 +28,6 @@ void MessageProc_WM_PAINT(HWND hWnd)
 	Image image(L"./透明png测试图片.png");
 	Graphics graphics(hdc);
 	graphics.DrawImage(&image, 0, 0);
-
 
 	EndPaint(hWnd, &ps);
 }
